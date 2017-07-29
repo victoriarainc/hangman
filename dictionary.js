52561b3618fb3267f5f410f102fed5aebac366b9
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-07-29 14:53:17 -0400

    organized files and input boiler plate code

---
+++ dictionary.js
@@ -0,0 +1,12 @@
+let dictionary = [
+  'gym socks',
+  'week old undies',
+  'moldy cheese',
+  'dust bunnies',
+  'unwashed dishes',
+  'dump',
+  'trash heap',
+  'hot garbage',
+  'diaper',
+  'toddler hands'
+]
